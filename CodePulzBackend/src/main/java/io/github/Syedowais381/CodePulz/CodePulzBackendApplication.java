a97f61bbde7bc0da94d9aaeb6f5bf8536f9278be
Dell <oquadri381@gmail.com>
2025-10-22 13:36:50 +0530

    Initial commit

---
+++ CodePulzBackend/src/main/java/io/github/Syedowais381/CodePulz/CodePulzBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.Syedowais381.CodePulz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePulzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePulzBackendApplication.class, args);
+	}
+
+}
---
+++ CodePulzBackend/src/test/java/io/github/Syedowais381/CodePulz/CodePulzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Syedowais381.CodePulz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePulzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
